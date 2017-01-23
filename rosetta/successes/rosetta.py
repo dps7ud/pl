4d524218d6fe57f0f070c20a5a24681267af1d93
dps7ud <dps7ud@virginia.edu>
2017-01-22 21:03:06 -0500

    Cleanup

--- rosetta/successes/rosetta.py
+++ rosetta/successes/rosetta.py
@@ -1,9 +1,7 @@
 import sys
-lst = list( zip( *([iter([x.strip() for x in sys.stdin.readlines()])] * 2 )))
+lst = list( zip( *(  [iter(  [x.strip() for x in sys.stdin.readlines()]  )]  * 2 )))
 def solve(tasks, pairs, ans):
-    if not tasks: 
-        print(*ans, sep='\n')
-        exit()
-    if sorted(list(tasks - set([x[0] for x in pairs]))): solve(tasks - set(sorted(list(tasks - set([x[0] for x in pairs])))[0]), [x for x in pairs if x[1] != sorted(list(tasks - set([x[0] for x in pairs])))[0]], ans + [sorted(list(tasks - set([x[0] for x in pairs])))[0]] )
-    else: print("cycle")
+    tasks or (print(*ans,sep='\n') or exit())
+    temp = sorted(list(tasks - set([x[0] for x in pairs])))
+    solve(tasks - set([temp[0]]), [x for x in pairs if x[1] != temp[0]], ans + [temp[0]] ) if temp else print("cycle")
 solve(set(sum(lst,())),lst,[])
---
+++ rosetta/tests/first.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo === 1:
+python3 ../successes/rosetta.py < t1.txt
+echo === 2:
+python3 ../successes/rosetta.py < t2.txt
+echo === 3:
+python3 ../successes/rosetta.py < t3.txt
+echo === 4:
+python3 ../successes/rosetta.py < t4.txt
+echo === 5:
+python3 ../successes/rosetta.py < t5.txt
+echo === 6:
+python3 ../successes/rosetta.py < t6.txt
+echo === 7:
+python3 ../successes/rosetta.py < t7.txt
