90a34b49df8f63ab2ea85c42a5f95faaae23dff9
dps7ud <dps7ud@virginia.edu>
2017-04-14 11:02:18 -0400

    keepinggg  ssttuuuuuuuuufffffffffffffffffffffffffffffff cause I need to reinstall ubuntu

---
+++ 5/check/first.ml
@@ -0,0 +1,58 @@
+type loc = string
+type id = loc * string
+type cool_type = id
+type class_map = attrib_class list
+(*and loc = string
+and id = loc * string
+and cool_type = id*)
+and attrib_class = id * attrib list
+and attrib = id * cool_type * (exp option)
+and exp =
+    | loc * exp_kind
+and exp_kind =
+    | Integer of int
+    | String of string
+(*type imp_map = imp_class list*)
+(*type parent_map = sub_super list*)
+
+
+let main () = begin
+    let fname = Sys.argv.(1) in
+    let fin = open_in fname in
+    let read() =
+        input_line fin (*fix for \r\n*)
+    in
+
+    let rec range k =
+        if k <= 0 then
+        else k :: (range (k-1))
+    in
+
+    let read_list worker =
+        let k = int_of_string (read()) in
+        let lst = range k in
+        List.map (fun _ -> worker () ) lst
+    in
+
+    (*read attribute map*)
+    let rec read_attrib_map () =
+        read_list read_attrib_class
+
+    and read_id () =
+        let loc = read() in
+        let name = read() in
+        (loc, name)
+
+    and read_attrib ()=
+        match read() with
+        | "no_initializer" -> None
+        | "initializer" -> read_exp()
+
+    and read_attrib_class () =
+        let cname = read_id () in
+        let attrib_list = read_list read_attrib in
+        (cname, attrib_list)
+
+
+end;;
+main();;
---
+++ 5/check/int.cl
@@ -0,0 +1,6 @@
+class Main{
+    i: Int <- 4 + 1;
+    main() : Object{
+        i
+    };
+};
